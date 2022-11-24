2a0611ee3d07576a2183df120cf4c3bb2fa6c6b7
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-24 18:57:49 +0200

    configure backend

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+Amplify.configure(config)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
