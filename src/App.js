73d2f1464ddec5758f22e533f172eaa45d3e2750
Siegfred Gamboa <siegy.gamboa@gmail.com>
2022-11-24 19:10:40 +0200

    refactor App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState, useEffect } from 'react'
+import { API } from 'aws-amplify'
+import { listProjects } from './graphql/queries'
+
 function App() {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    fetchProjects()
+  }, [])
+  async function fetchProjects() {
+    const apiData = await API.graphql({ query: listProjects })
+    const projectsFromAPI = apiData.data.listProjects.items
+    setProjects(projectsFromAPI)
+    console.log(projectsFromAPI)
+  }
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Hello Siegfred</h1>
+      <button onClick={fetchProjects}>Fetch</button>
+      {projects.map((project) => (
+        <h1 key={project.id}>{project.title}</h1>
+      ))}
     </div>
   )
 }
